93cf646fd7522e23eaf8bdb3c1fc9508f6e1e610
Alena Pumprová <alenapumprova@seznam.cz>
2025-09-22 01:17:48 +0200

    moving Shelf to the own component

--- escape/src/components/Room.js
+++ escape/src/components/Room.js
@@ -1,20 +1,14 @@
 import { useState, useEffect, useRef, useCallback } from "react";
 import "./Room.css";
 import CodeLock from "./CodeLock";
+import Shelf from "./Shelf";
 /** Sounds */
 import switchSound from "../sounds/light-switch-382712.mp3";
 import Voices from "../sounds/015922_whispers-39schizophrenic39-or-ghost-like-voices-56253.mp3"
 import Ghost from "../sounds/ghost-6979.mp3";
-import Phone from "../sounds/old-rotary-phone-1-296475.mp3";
 import Alien from "../sounds/alien-underworld-sound-287342.mp3";
 import Mirror from "../sounds/creepy-moan-87456.mp3";
-import Book from "../sounds/flipping-book-101929.mp3";
-import FirstAid from "../sounds/old-metal-lunch-box-71223.mp3";
-import MetalBox from "../sounds/box-crash-106687.mp3";
 import CardboardBox from "../sounds/cardboard-box-open-182560.mp3";
-import Cassette from "../sounds/cassette-34173.mp3"
-import Ball from "../sounds/small-ball-393217.mp3"
-import Pickup from "../sounds/item-removed-from-box-140495.mp3"
 import Door from "../sounds/door-handle-1-401153.mp3"
 import Rug from "../sounds/object-dragged-on-carpet-140497.mp3"
 import Paper from "../sounds/paper-rustle-81855.mp3"
@@ -625,44 +619,32 @@ const updateView = (direction) => {
     };
     
     const initItems = () => {
-      const roomItems = document.querySelectorAll("#room [data-comment]:not(.switch):not(.lock):not(.door)");
+  // ✅ necháme jen hint button
+  const hints = [
+    "Talk to the old spirits.",
+    "Admire the art.",
+    "Get the code first.",
+    "Don't forget to look into boxes.",
+    "You need to search all the stuff.",
+    "Play sports.",
+    "Read the book.",
+    "It's always better with lights on.",
+    "Don't be afraid of ghosts.", 
+    "Don't forget to check under the rug.",
+  ];
 
-      roomItems.forEach((item) => {
-        item.onclick = (e) => {
-          e.stopPropagation();
-          const comment = item.getAttribute("data-comment");
-          if (comment) showComment(comment);
+  const hintBtn = document.getElementById("hint");
+  if (hintBtn) {
+    hintBtn.onclick = () => {
+      const random = hints[Math.floor(Math.random() * hints.length)];
+      showComment(random, "hint");
 
-          // 📝 The click count
-          // const clicks = parseInt(localStorage.getItem("itemsClicked") || "0", 10);
-          // localStorage.setItem("itemsClicked", clicks + 1);
-        };
-      });
-
-      const hints = [
-        "Talk to the old spirits.",
-        "Admire the art.",
-        "Get the code first.",
-        "Don't forget to look into boxes.",
-        "You need to search all the stuff.",
-        "Play sports.",
-        "Read the book.",
-        "It's always better with lights on.",
-        "Don't be afraid of ghosts.", 
-        "Don't forget to check under the rug.",
-      ];
-
-      const hintBtn = document.getElementById("hint");
-      if (hintBtn) {
-        hintBtn.onclick = () => {
-          const random = hints[Math.floor(Math.random() * hints.length)];
-          showComment(random, "hint");
-          // 📝 Save hints used
-          const used = parseInt(localStorage.getItem("hintsUsed") || "0", 10);
-          localStorage.setItem("hintsUsed", used + 1);
-        };
-      }
+      // 📝 Save hints used
+      const used = parseInt(localStorage.getItem("hintsUsed") || "0", 10);
+      localStorage.setItem("hintsUsed", used + 1);
     };
+  }
+};
 
     const init = () => {
       requestAnimationFrame(() => updateView());
@@ -776,152 +758,13 @@ const updateView = (direction) => {
                 }}>
               <span className="visually-hidden">A Lightbox sign laying in the corner</span>
             </div>
-            <div className="cube shelf">
-              <div className="cube shelf-level level-1">
-                <div 
-                  className="item skull" 
-                  data-title="A Shiny Skull" 
-                  data-comment="Wow, it has full set of teeth!"
-                  onClick={(e) => {
-                    incrementItemClicks("skull");
-                    playSound(Pickup);
-                    triggerVibration(30);
-                    const msg = e.currentTarget.getAttribute("data-comment");
-                    if (msg) showComment(msg);
-                  }}
-                >
-                  <div className="item-inner">
-                    <span className="visually-hidden">A metalic skull</span>
-                  </div>
-                </div>
-
-                <div
-                  className="item cassette"
-                  data-title="Some old cassette"
-                  data-comment="What do we have there? Bryan Adams - Summer of ... Oh no. Stuck forever in my head."
-                  onClick={(e) => {
-                    playSound(Cassette, {duration: 2});
-                    incrementItemClicks("cassette");
-                    const msg = e.currentTarget.getAttribute("data-comment");
-                    if (msg) showComment(msg);
-                    triggerVibration(30);
-                  }}
-                >
-                  <div className="item item-inner">
-                    <span className="visually-hidden">Old cassette</span>
-                  </div>
-                </div>
-                
-                <div
-                  className="item ball"
-                  data-title="A random billiard ball"  
-                  data-comment="What number is this? Does it matter? It's just a ball."
-                  onClick={(e) => {
-                    playSound(Ball);
-                    incrementItemClicks("ball");
-                    const msg = e.currentTarget.getAttribute("data-comment");
-                    if (msg) showComment(msg);
-                    triggerVibration(30);
-                  }}
-                >
-                  <div className="item item-inner">
-                    <span className="visually-hidden">Black billiard ball with number 8</span>
-                  </div>
-                </div>
-              </div>
-              
-              <div className="cube shelf-level level-2">
-                <div 
-                  className="item globe egg" 
-                  data-title="An old globe" 
-                  data-comment="Where is Czechia? Europe, right? Damn, I hate geography. Wait, what… is there a mic in the stand?"
-                  onClick={(e) => {
-                    playSound(Pickup);
-                    incrementItemClicks("globe");
-                    unlockEasterEgg("globe"); // save to LocalStorage
-                    const msg = e.currentTarget.getAttribute("data-comment");
-                    if (msg) showComment(msg, "easter-egg");
-                    triggerVibration(30);
-                  }}
-                >
-                  <div className="item-inner item">
-                    <span className="visually-hidden">An Old Globe</span>
-                  </div>
-                </div>
-                
-                <div
-                  className="item phone"
-                  data-title="Ancient Technology"
-                  data-comment="Maybe it still works? I'll call my mom. 6-0-2 Oh no! My finger got stuck!"
-                  onClick={(e) => {
-                    playSound(Phone, {duration: 4.2});
-                    incrementItemClicks("phone");
-                    const msg = e.currentTarget.getAttribute("data-comment");
-                    if (msg) showComment(msg);
-                    triggerVibration(30);
-                  }}
-                >
-                  <div className="item-inner item">
-                    <span className="visually-hidden">Old rotary phone</span>
-                  </div>
-                </div>
-                
-                <div
-                  className="item book"
-                  data-title="An old book"
-                  data-comment="He gazed up at the enormous face. Forty years it had taken him to learn what kind of smile was hidden beneath the dark moustache. O cruel, needless misunderstanding! O stubborn, self-willed exile from the loving breast! Two gin-scented tears trickled down the sides of his nose. But it was all right, everything was all right, the struggle was finished. He had won the victory over himself. He loved Big Brother. I know this book!"
-                  onClick={(e) => {
-                    playSound(Book);
-                    incrementItemClicks("book");
-                    const msg = e.currentTarget.getAttribute("data-comment");
-                    if (msg) showComment(msg);
-                    triggerVibration(30);
-                  }}
-                >
-                  <div className="item-inner item">
-                    <span className="visually-hidden">Old book</span>
-                  </div>
-                </div>
-              </div>
-              
-              <div className="cube shelf-level level-3">
-                <div
-                  className="item item-cube left-cube"
-                  id="hover-not"
-                  data-title="An army Medical Kit"
-                  data-comment="I hope there is something useful inside. Ouch, my finger! Thank goodness I have this first aid kit."
-                  onClick={(e) => {
-                    playSound(FirstAid, {duration: 6});
-                    incrementItemClicks("first-aid");
-                    const msg = e.currentTarget.getAttribute("data-comment");
-                    if (msg) showComment(msg);
-                    triggerVibration(30);
-                  }}
-                >
-                  <div className="cube medical-chest item" id="hover-not">
-                    <span className="visually-hidden">Old army medical kit</span>
-                  </div>
-                </div>
-                
-                <div
-                  className="item item-cube right-cube"
-                  id="hover-not"
-                  data-title="An army metal box"
-                  data-comment="No way I could open this! The lock looks rusted solid and the whole thing feels like a ton of bricks."
-                  onClick={(e) => {
-                    playSound(MetalBox);
-                    incrementItemClicks("metal-box");
-                    const msg = e.currentTarget.getAttribute("data-comment");
-                    if (msg) showComment(msg);
-                    triggerVibration(30);
-                  }}
-                >
-                  <div className="cube metal-box item" id="hover-not">
-                    <span className="visually-hidden">Dark metal box with black and yellow striped edges</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Shelf
+              incrementItemClicks={incrementItemClicks}
+              playSound={playSound}
+              triggerVibration={triggerVibration}
+              showComment={showComment}
+              unlockEasterEgg={unlockEasterEgg}
+            />
           </div>
           
           <div className="wall wall-back">            
--- escape/src/components/Shelf.js
+++ escape/src/components/Shelf.js
@@ -1,70 +1,161 @@
-import React, { useEffect, useRef } from "react";
-import "./Shelf.css"
+import "./Shelf.css";
+import Phone from "../sounds/old-rotary-phone-1-296475.mp3";
+import Book from "../sounds/flipping-book-101929.mp3";
+import FirstAid from "../sounds/old-metal-lunch-box-71223.mp3";
+import MetalBox from "../sounds/box-crash-106687.mp3";
+import Cassette from "../sounds/cassette-34173.mp3"
+import Ball from "../sounds/small-ball-393217.mp3"
+import Pickup from "../sounds/item-removed-from-box-140495.mp3"
 
-const Shelf = ({ onItemClick, onTooltip, onTooltipHide }) => {
-  const rootRef = useRef(null);
-
-  useEffect(() => {
-    const root = rootRef.current;
-    if (!root) return;
-
-    // Find all cube elements that need faces
-    const cubes = root.querySelectorAll(".cube");
-    cubes.forEach((cube) => {
-      if (cube.dataset.facesReady === "1") return;
-      
-      ["top", "left", "front", "right", "back", "bottom"].forEach((face) => {
-        const hasDirectFace = Array.from(cube.children)
-          .some(ch => ch.classList.contains(`cube-${face}`));
-        if (!hasDirectFace) {
-          const el = document.createElement("div");
-          el.className = `cube-${face}`;
-          cube.appendChild(el);
-        }
-      });
-
-      cube.dataset.facesReady = "1";
-    });
-  }, []);
-
+const Shelf = ({ onItemClick, onTooltip, onTooltipHide, incrementItemClicks,playSound, triggerVibration,showComment, unlockEasterEgg }) => {
   return (
-    <article className="bookshelf" ref={rootRef}>
-      <div className="cube shelf">
-        <div className="cube shelf-level level-1">
-          <div className="item gift" data-title="A Little Gift" data-item="gift" data-comment="What's inside?">
-            <div className="item-inner" />
-          </div>
-          <div className="item vhs" data-title="Strange Black Tape" data-item="tape" data-comment="It's not very sticky.">
-            <div className="item-inner" />
-          </div>
-          <div className="item mod" data-title="Valuable Mod Chip" data-item="chip" data-comment="Upgraded!">
-            <div className="item-inner" />
-          </div>
-        </div>
-
-        <div className="cube shelf-level level-2">
-          <div className="item milk" data-title="Spoiled Milk" data-item="milk" data-comment="Lunch.">
-            <div className="item-inner" />
-          </div>
-          <div className="item scraps" data-title="Ancient Technology" data-item="scraps" data-comment="Game Saved!">
-            <div className="item-inner" />
-          </div>
-          <div className="item book" data-title="Some old book" data-item="book" data-comment="Maybe it's edible?">
-            <div className="item-inner" />
-          </div>
-        </div>
-
-        <div className="cube shelf-level level-3">
-          <div className="item item-cube left-cube" data-title="Minecraft!" data-comment="<q class='green'>Creeper? Aw man!</q>">
-            <div className="cube minecraft" />
-          </div>
-          <div className="item item-cube right-cube" data-title="Cobblestone (64x)" data-comment="No way I could fit that much stone in my inventory!">
-            <div className="cube cobblestone" />
-          </div>
-        </div>
-      </div>
-    </article>
-  );
+    <div className="cube shelf">
+                  <div className="cube shelf-level level-1">
+                    <div 
+                      className="item skull" 
+                      data-title="A Shiny Skull" 
+                      data-comment="Wow, it has full set of teeth!"
+                      onClick={(e) => {
+                        incrementItemClicks("skull");
+                        playSound(Pickup);
+                        triggerVibration(30);
+                        const msg = e.currentTarget.getAttribute("data-comment");
+                        if (msg) showComment(msg);
+                      }}
+                    >
+                      <div className="item-inner">
+                        <span className="visually-hidden">A metalic skull</span>
+                      </div>
+                    </div>
+    
+                    <div
+                      className="item cassette"
+                      data-title="Some old cassette"
+                      data-comment="What do we have there? Bryan Adams - Summer of ... Oh no. Stuck forever in my head."
+                      onClick={(e) => {
+                        playSound(Cassette, {duration: 2});
+                        incrementItemClicks("cassette");
+                        const msg = e.currentTarget.getAttribute("data-comment");
+                        if (msg) showComment(msg);
+                        triggerVibration(30);
+                      }}
+                    >
+                      <div className="item item-inner">
+                        <span className="visually-hidden">Old cassette</span>
+                      </div>
+                    </div>
+                    
+                    <div
+                      className="item ball"
+                      data-title="A random billiard ball"  
+                      data-comment="What number is this? Does it matter? It's just a ball."
+                      onClick={(e) => {
+                        playSound(Ball);
+                        incrementItemClicks("ball");
+                        const msg = e.currentTarget.getAttribute("data-comment");
+                        if (msg) showComment(msg);
+                        triggerVibration(30);
+                      }}
+                    >
+                      <div className="item item-inner">
+                        <span className="visually-hidden">Black billiard ball with number 8</span>
+                      </div>
+                    </div>
+                  </div>
+                  
+                  <div className="cube shelf-level level-2">
+                    <div 
+                      className="item globe egg" 
+                      data-title="An old globe" 
+                      data-comment="Where is Czechia? Europe, right? Damn, I hate geography. Wait, what… is there a mic in the stand?"
+                      onClick={(e) => {
+                        playSound(Pickup);
+                        incrementItemClicks("globe");
+                        unlockEasterEgg("globe"); // save to LocalStorage
+                        const msg = e.currentTarget.getAttribute("data-comment");
+                        if (msg) showComment(msg, "easter-egg");
+                        triggerVibration(30);
+                      }}
+                    >
+                      <div className="item-inner item">
+                        <span className="visually-hidden">An Old Globe</span>
+                      </div>
+                    </div>
+                    
+                    <div
+                      className="item phone"
+                      data-title="Ancient Technology"
+                      data-comment="Maybe it still works? I'll call my mom. 6-0-2 Oh no! My finger got stuck!"
+                      onClick={(e) => {
+                        playSound(Phone, {duration: 4.2});
+                        incrementItemClicks("phone");
+                        const msg = e.currentTarget.getAttribute("data-comment");
+                        if (msg) showComment(msg);
+                        triggerVibration(30);
+                      }}
+                    >
+                      <div className="item-inner item">
+                        <span className="visually-hidden">Old rotary phone</span>
+                      </div>
+                    </div>
+                    
+                    <div
+                      className="item book"
+                      data-title="An old book"
+                      data-comment="He gazed up at the enormous face. Forty years it had taken him to learn what kind of smile was hidden beneath the dark moustache. O cruel, needless misunderstanding! O stubborn, self-willed exile from the loving breast! Two gin-scented tears trickled down the sides of his nose. But it was all right, everything was all right, the struggle was finished. He had won the victory over himself. He loved Big Brother. I know this book!"
+                      onClick={(e) => {
+                        playSound(Book);
+                        incrementItemClicks("book");
+                        const msg = e.currentTarget.getAttribute("data-comment");
+                        if (msg) showComment(msg);
+                        triggerVibration(30);
+                      }}
+                    >
+                      <div className="item-inner item">
+                        <span className="visually-hidden">Old book</span>
+                      </div>
+                    </div>
+                  </div>
+                  
+                  <div className="cube shelf-level level-3">
+                    <div
+                      className="item item-cube left-cube"
+                      id="hover-not"
+                      data-title="An army Medical Kit"
+                      data-comment="I hope there is something useful inside. Ouch, my finger! Thank goodness I have this first aid kit."
+                      onClick={(e) => {
+                        playSound(FirstAid, {duration: 6});
+                        incrementItemClicks("first-aid");
+                        const msg = e.currentTarget.getAttribute("data-comment");
+                        if (msg) showComment(msg);
+                        triggerVibration(30);
+                      }}
+                    >
+                      <div className="cube medical-chest item" id="hover-not">
+                        <span className="visually-hidden">Old army medical kit</span>
+                      </div>
+                    </div>
+                    
+                    <div
+                      className="item item-cube right-cube"
+                      id="hover-not"
+                      data-title="An army metal box"
+                      data-comment="No way I could open this! The lock looks rusted solid and the whole thing feels like a ton of bricks."
+                      onClick={(e) => {
+                        playSound(MetalBox);
+                        incrementItemClicks("metal-box");
+                        const msg = e.currentTarget.getAttribute("data-comment");
+                        if (msg) showComment(msg);
+                        triggerVibration(30);
+                      }}
+                    >
+                      <div className="cube metal-box item" id="hover-not">
+                        <span className="visually-hidden">Dark metal box with black and yellow striped edges</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+  )
 };
 
 export default Shelf;
